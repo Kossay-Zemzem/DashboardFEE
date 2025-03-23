33b4767ad459d74c634ec92cd9176fe7acb9e3b5
Kossay-Zemzem <kossayzemzem@gmail.com>
2025-03-23 16:07:18 +0100

    Fixed header title after some temporary testing adjustments

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  title:string = 'Demandes de participation';
-  leftOffsetTcss:string="left-[22rem]" //offset of the yellow box decoration
+  title:string = 'Membres de comités';
+  leftOffsetTcss:string="left-[14.7rem]" //offset of the yellow box decoration
   topOffsetTcss:string="top-[0.6rem]" //offset of the yellow box decoration
   yellowBoxWidthTcss:string="w-40" //width of the yellow box decoration
 
