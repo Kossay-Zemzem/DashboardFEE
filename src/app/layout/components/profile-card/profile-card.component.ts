1e9a91ee1e64927de87a6c89444f6494c9fab558
Kossay-Zemzem <kossayzemzem@gmail.com>
2025-03-23 13:03:31 +0100

    Profile card is fully reusable now.

--- src/app/layout/components/profile-card/profile-card.component.ts
+++ src/app/layout/components/profile-card/profile-card.component.ts
@@ -9,6 +9,9 @@ export class ProfileCardComponent {
   nom:string="MEKKI";
   prenom:string="Mohamed";
   email:string="Mohamed.Mekki@enicar.ucar.tn";
+  imageSrc:string="../../../../assets/images/Placeholder/blank-profile-picture.png";
+  /*"../../../../assets/images/Placeholder/blank-profile-picture.png"
+            class="h-12 w-12 rounded-lg object-left-bottom"*/
   constructor(){
 
   }
